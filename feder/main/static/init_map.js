d2d68cb5ff8982f897b2805f57b73724def65208
Andrzej Kręcicki <andrzej@krecicki.pl>
2015-08-18 21:28:55 +0200

    Remove unused tooltip initialization code

--- feder/main/static/init_map.js
+++ feder/main/static/init_map.js
@@ -2,5 +2,4 @@
     $("#pl_map path").on("click", function() {
         document.location.pathname = $(this).data("href")
     });
-    $('[data-toggle="tooltip"]').tooltip({'container': 'body'});
 })(jQuery);
