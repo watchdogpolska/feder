63c3d8ac49fdaab5c9e414de6a093c7e12369fdd
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-08-21 23:08:37 +0200

    Fix rght for tasks

--- feder/tasks/tests.py
+++ feder/tasks/tests.py
@@ -23,6 +23,7 @@ class CaseTestCase(TestCase):
         jst = AutoFixture(JednostkaAdministracyjna,
             field_values={'updated_on': '2015-02-12'},
             generate_fk=True).create_one(commit=False)
+        jst.rght = 10
         jst.save()
         JednostkaAdministracyjna.objects.rebuild()
         return jst
