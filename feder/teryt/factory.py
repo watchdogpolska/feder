5d83bea62eaee6581fabc30ab40868d71cb53957
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-08-29 03:52:55 +0200

    Drop randomity in teryt.factory, use counter by global variable

--- feder/teryt/factory.py
+++ feder/teryt/factory.py
@@ -1,11 +1,14 @@
-from random import randint
 from .models import JednostkaAdministracyjna as JST
 from .models import Category
 
+TERYT_COUNTER = 0
+
 
 def factory_jst():
+    global TERYT_COUNTER
+    TERYT_COUNTER += 1
     category = Category.objects.create(name="X", level=1)
-    return JST.objects.create(name="X", id=randint(0, 1000),
+    return JST.objects.create(name="X", id=TERYT_COUNTER,
                               category=category,
                               updated_on='2015-05-12',
                               active=True)
