8fb81cda9803fa0e8e58efc9d6c3fc73cf8c236d
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-18 03:57:04 +0200

    Initial teryt migrations, move teryt templates

---
+++ feder/teryt/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('teryt_tree', '0005_auto_20150816_0342'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='JST',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('teryt_tree.jednostkaadministracyjna',),
+        ),
+    ]
