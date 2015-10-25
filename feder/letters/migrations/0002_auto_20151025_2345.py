f223001ea6a885a7c732fe26e22217df77faf60a
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-10-26 00:52:58 +0100

    Fix django_mailbox dependency in letters

--- feder/letters/migrations/0002_auto_20151025_2345.py
+++ feder/letters/migrations/0002_auto_20151025_2345.py
@@ -8,7 +8,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_mailbox', '0005_auto_20150829_2235'),
+        ('django_mailbox', '0004_bytestring_to_unicode'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('letters', '0001_initial'),
         ('cases', '0004_case_user'),
