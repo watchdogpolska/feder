2a825053311691ef69857fcdc6908eac51ae48fe
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-08-23 17:37:38 +0200

    Update tests for alerts

--- feder/alerts/tests.py
+++ feder/alerts/tests.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from guardian.shortcuts import assign_perm
 
 from feder.monitorings.factories import factory_monitoring
 
@@ -13,49 +14,51 @@ try:
 except ImportError:
     from django.contrib.auth.models import User
 
+from feder.users.factories import UserFactory
 
-class SetUpMixin(object):
+
+class ObjectMixin(object):
     def setUp(self):
-        self.user = User.objects.create_user(username="john", password="top_secret")
-        self.quest = User.objects.create_user(username="quest", password="top_secret")
+        self.user = UserFactory(username="john", password="pass")
         self.monitoring = factory_monitoring(self.user)
         self.alert = Alert.objects.create(reason="lorem",
                                           author=self.user,
                                           monitoring=self.monitoring)
 
 
-class PermCheckMixin(SetUpMixin):
+class PermCheckMixin(ObjectMixin):
     anonymous_status = 302
     non_permitted_status = 403
     permitted_status = 200
-
-    def login(self):
-        self.client.login(username="quest", password="top_secret")
-
-    def login_permitted(self):
-        self.client.login(username="john", password="top_secret")
+    permission = []
 
     def test_user_anonymous(self):
         response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, self.anonymous_status)
 
     def test_user_non_permitted(self):
-        self.login()
+        self.client.login(username="john", password="pass")
         response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, self.non_permitted_status)
 
     def test_user_permitted(self):
-        self.login_permitted()
+        for perm in self.permission:
+            assign_perm(perm, self.user, self.monitoring)
+        self.client.login(username="john", password="pass")
         response = self.client.get(self.get_url())
         self.assertEqual(response.status_code, self.permitted_status)
 
 
 class ListPermTestCase(PermCheckMixin, TestCase):
+    permission = ['monitorings.view_alert', ]
+
     def get_url(self):
         return reverse('alerts:list', kwargs={"monitoring": self.monitoring.pk})
 
 
 class DetailsPermTestCase(PermCheckMixin, TestCase):
+    permission = ['monitorings.view_alert', ]
+
     def get_url(self):
         return self.alert.get_absolute_url()
 
@@ -69,10 +72,14 @@ class CreatePermTestCase(PermCheckMixin, TestCase):
 
 
 class UpdatePermTestCase(PermCheckMixin, TestCase):
+    permission = ['monitorings.change_alert', ]
+
     def get_url(self):
         return reverse('alerts:update', kwargs={"pk": self.alert.pk})
 
 
 class SwitchPermTestCase(PermCheckMixin, TestCase):
+    permission = ['monitorings.change_alert', ]
+
     def get_url(self):
         return reverse('alerts:status', kwargs={"pk": self.alert.pk})
