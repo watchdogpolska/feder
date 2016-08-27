999600e71265986d81aa6f6b8281389f86324868
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-08-27 12:58:41 +0200

    Add HomeViewTestCase

--- feder/main/tests.py
+++ feder/main/tests.py
@@ -1,3 +1,8 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeViewTestCase(TestCase):
+    def test_status_code(self):
+        response = self.client.get(reverse('main:home'))
+        self.assertEqual(response.status_code, 200)
