cfe78dabea226e24928d26183f4b135c52b64663
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-08-27 12:45:52 +0200

    Clean up form in CaseForm

--- feder/cases/forms.py
+++ feder/cases/forms.py
@@ -11,11 +11,8 @@ class CaseForm(SingleButtonMixin, UserKwargModelFormMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.monitoring = kwargs.pop('monitoring', None)
         super(CaseForm, self).__init__(*args, **kwargs)
-
-    def save(self, *args, **kwargs):
         if self.monitoring:
             self.instance.monitoring = self.monitoring
-        super(CaseForm, self).save(*args, **kwargs)
 
     class Meta:
         model = Case
