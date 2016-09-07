09c2b0768c052d44e96cba834feef005326ca972
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-09-07 23:09:48 +0200

    Add filters in institutions

--- feder/institutions/viewsets.py
+++ feder/institutions/viewsets.py
@@ -1,7 +1,17 @@
-from rest_framework import viewsets
+import django_filters
+from rest_framework import filters, viewsets
 
 from .models import Email, Institution, Tag
 from .serializers import EmailSerializer, InstitutionSerializer, TagSerializer
+from teryt_tree.rest_framework_ext.viewsets import custom_area_filter
+
+
+class InstitutionFilter(filters.FilterSet):
+    jst = django_filters.CharFilter(action=custom_area_filter)
+
+    class Meta:
+        model = Institution
+        fields = ['tags', 'jst']
 
 
 class InstitutionViewSet(viewsets.ModelViewSet):
@@ -11,6 +21,8 @@ class InstitutionViewSet(viewsets.ModelViewSet):
                 with_accurate_email().
                 all())
     serializer_class = InstitutionSerializer
+    filter_backends = (filters.DjangoFilterBackend, )
+    filter_class = InstitutionFilter
 
 
 class TagViewSet(viewsets.ModelViewSet):
