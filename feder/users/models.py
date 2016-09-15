632f70d64bac45365974db834a3a6ddcb16e13ad
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-09-15 03:49:26 +0200

    Add GuardianModelMixin in users/models.py

--- feder/users/models.py
+++ feder/users/models.py
@@ -3,13 +3,11 @@ from __future__ import absolute_import, unicode_literals
 
 from django.contrib.auth.models import AbstractUser
 from django.utils.encoding import python_2_unicode_compatible
+from guardian.mixins import GuardianUserMixin
 
-
-# from django.db import models
-# from django.utils.translation import ugettext_lazy as _
 
 @python_2_unicode_compatible
-class User(AbstractUser):
+class User(GuardianUserMixin, AbstractUser):
 
     def __str__(self):
         return self.username
