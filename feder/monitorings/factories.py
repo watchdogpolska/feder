dd7513f4146679d11aff6d528f11927131dc692f
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-09-16 23:55:27 +0200

    Add description and template to MonitoringFactory

--- feder/monitorings/factories.py
+++ feder/monitorings/factories.py
@@ -6,6 +6,9 @@ import factory
 class MonitoringFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: 'monitoring-%04d' % n)
     user = factory.SubFactory(UserFactory)
+    description = factory.Sequence(lambda n: 'description no.%04d' % n)
+    template = factory.Sequence(lambda n:
+                                'template no.%04d. reply to {{EMAIL}}' % n)
 
     class Meta:
         model = Monitoring
