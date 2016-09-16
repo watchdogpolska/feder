f9c91a229c4e06bfca85b0ccf1c673f61fa051aa
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-09-16 23:20:18 +0200

    Clean up feder.tasks.tests

--- feder/tasks/tests.py
+++ feder/tasks/tests.py
@@ -1,28 +1,17 @@
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from guardian.shortcuts import assign_perm
 
-from feder.cases.models import Case
-from feder.institutions.factories import InstitutionFactory
-from feder.monitorings.models import Monitoring
-from feder.questionaries.models import Questionary
-from feder.tasks.models import Task
+from feder.main.mixins import PermissionStatusMixin
 from feder.users.factories import UserFactory
+
+from .factories import TaskFactory
 
 
 class ObjectMixin(object):
     def setUp(self):
         self.user = UserFactory(username='john')
-        self.monitoring = Monitoring.objects.create(name="Lor", user=self.user)
-        self.questionary = Questionary.objects.create(title="blabla",
-                                                      monitoring=self.monitoring)
-        self.institution = InstitutionFactory()
-        self.case = Case.objects.create(name="blabla",
-                                        monitoring=self.monitoring,
-                                        institution=self.institution,
-                                        user=self.user)
-        self.task = Task.objects.create(case=self.case,
-                                        questionary=self.questionary)
+        self.task = TaskFactory()
+        self.permission_object = self.task.case.monitoring
 
 
 class CaseTestCase(ObjectMixin, TestCase):
@@ -35,47 +24,21 @@ class CaseTestCase(ObjectMixin, TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class PermCheckMixin(ObjectMixin):
-    url = None
-    template_name = 'tasks/task_form.html'
-    permission = []
-
-    def get_url(self):
-        raise NotImplementedError()
-
-    def test_anonymous_user(self):
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 302)
-
-    def test_non_permitted_user(self):
-        self.client.login(username='john', password='pass')
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 403)
-
-    def test_permitted_user(self):
-        for perm in self.permission:
-            assign_perm(perm, self.user, self.monitoring)
-        self.client.login(username='john', password='pass')
-        response = self.client.get(self.get_url())
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, self.template_name)
-
-
-class CreateViewPermTestCase(PermCheckMixin, TestCase):
+class TaskCreateViewTestCase(ObjectMixin, PermissionStatusMixin, TestCase):
     permission = ['monitorings.add_task', ]
 
     def get_url(self):
-        return reverse('tasks:create', kwargs={'case': str(self.case.pk)})
+        return reverse('tasks:create', kwargs={'case': str(self.task.case.pk)})
 
 
-class UpdateViewPermTestCase(PermCheckMixin, TestCase):
+class TaskUpdateViewPermTestCase(ObjectMixin, PermissionStatusMixin, TestCase):
     permission = ['monitorings.change_task', ]
 
     def get_url(self):
         return reverse('tasks:update', kwargs={'pk': self.task.pk})
 
 
-class DeleteViewPermTestCase(PermCheckMixin, TestCase):
+class TaskDeleteViewTestCase(ObjectMixin, PermissionStatusMixin, TestCase):
     permission = ['monitorings.delete_task', ]
     template_name = 'tasks/task_confirm_delete.html'
 
