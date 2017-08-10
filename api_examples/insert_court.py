5794763bbc61c272558c43d101e95191105785ea
Adam Dobrawy <naczelnik@jawnosc.tk>
2017-08-11 01:13:57 +0200

    Update courts import to extract level and class-based command

--- api_examples/insert_court.py
+++ api_examples/insert_court.py
@@ -1,4 +1,5 @@
 #!/bin/python2.7
+# -*- coding: utf-8 -*-
 """
 A tool to insert institutions from CSV files.
 
@@ -13,7 +14,7 @@ Example usage:
 To run help text use:
 $ python insert_court.py -h
 """
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 import argparse
 import os
@@ -32,9 +33,6 @@ try:
 except ImportError:
     from urllib.parse import urljoin
 
-REQUIRED_FIELDS = ['parent__name', 'name', 'district', 'appeal', 'street',
-                   'postcode', 'phone', 'fax', 'email', 'active', 'regon']
-
 requests_cache.configure()
 
 
@@ -50,109 +48,131 @@ def environ(key, bool=False):
 if not bool(environ('GUSREGON_SANDBOX')):
     sys.stderr.write("You are using sandbox mode for the REGON database. Data may be incorrect. "
                      "Set the environemnt variable GUSREGON_API_KEY correctly.")
-gus = GUS(api_key=environ('GUSREGON_API_KEY'), sandbox=environ('GUSREGON_SANDBOX', True))
-s = requests.Session()
-
-JST_VOIVODESHIP_KEYS = ['adsiedzwojewodztwo_symbol', 'adkorwojewodztwo_symbol']
-JST_COUNTY_KEYS = ['adsiedzpowiat_symbol', 'adkorpowiat_symbol', ]
-JST_COMMUNITY_KEYS = ["adsiedzgmina_symbol", 'adkorgmina_symbol', ]
 
 
-def get_jst_id(data):
-    for a, b, c in zip(JST_VOIVODESHIP_KEYS, JST_COUNTY_KEYS, JST_COMMUNITY_KEYS):
-        if a in data and b in data and c in data:
-            value = data[a] + data[b] + data[c]
-            if value:
-                return value
+class Command(object):
+    JST_VOIVODESHIP_KEYS = ['adsiedzwojewodztwo_symbol', 'adkorwojewodztwo_symbol']
+    JST_COUNTY_KEYS = ['adsiedzpowiat_symbol', 'adkorpowiat_symbol', ]
+    JST_COMMUNITY_KEYS = ["adsiedzgmina_symbol", 'adkorgmina_symbol', ]
+    REQUIRED_FIELDS = ['parent__name', 'name', 'district', 'appeal', 'street',
+                       'postcode', 'phone', 'fax', 'email', 'active', 'regon']
 
+    def __init__(self, argv):
+        self.gus = GUS(api_key=environ('GUSREGON_API_KEY'), sandbox=environ('GUSREGON_SANDBOX', True))
+        self.s = requests.Session()
+        self.argv = argv
+        self.args = self.get_build_args(argv[1:])
+        self.s.auth = (self.args.user, self.args.password)
 
-def get_court(host, name):
-    response = s.get(url=urljoin(host, "/api/institutions/"), params={'name': name}).json()
-    if not response['results']:
-        import ipdb; ipdb.set_trace();
-    return response['results'][0]['pk']
+    def get_build_args(self, argv):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--input',
+                            required=True,
+                            type=argparse.FileType('r'),
+                            help="Input CSV-file")
+        parser.add_argument('--host',
+                            required=True,
+                            help="Host of instance")
+        parser.add_argument('--user',
+                            required=True,
+                            help="User to authentication")
+        parser.add_argument('--password',
+                            required=True,
+                            help="Password to authentication")
+        return parser.parse_args(argv)
 
+    def _get_jst_id(self, data):
+        for a, b, c in zip(self.JST_VOIVODESHIP_KEYS,
+                           self.JST_COUNTY_KEYS,
+                           self.JST_COMMUNITY_KEYS):
+            if a in data and b in data and c in data:
+                value = data[a] + data[b] + data[c]
+                if value:
+                    return value
 
-def insert_row(host, name, email, regon, tags, **extra):
-    data = gus.search(regon=regon)
-    if not data:
-        print("Unable find REGON {} for {}".format(regon, name), file=sys.stderr)
-        return
-    response = s.post(url=urljoin(host, "/api/institutions/"), json={
-        "name": name,
-        "tags": tags,
-        "jst": normalize_jst(get_jst_id(data)),
-        "email": email,
-        "extra": extra,
-        "regon": regon
-    })
-    if response.status_code == 500:
-        print(name.encode('utf-8'), " response 500", response.status_code, ":", file=sys.stderr)
-        print(response.text, file=sys.stderr)
-        return
+    def _get_court(self, host, name):
+        response = self.s.get(url=urljoin(host, "/api/institutions/"), params={'name': name}).json()
+        if not response['results']:
+            import ipdb; ipdb.set_trace();
+        return response['results'][0]['pk']
 
-    json = response.json()
-    if response.status_code == 201:
-        print(name.encode('utf-8'), " created as PK", json['pk'])
-    else:
-        print(name.encode('utf-8'), "response ", response.status_code, ":", file=sys.stderr)
-        print(json, file=sys.stderr)
+    def insert_row(self, host, name, email, regon, **extra):
+        data = self.gus.search(regon=regon)
+        if not data:
+            print("Unable find REGON {} for {}".format(regon, name), file=sys.stderr)
+            return
 
+        tags = []
+        for keyword in ['Sąd Apelacyjny', 'Sąd Okręgowy', 'Sąd Rejonowy']:
+            if keyword in name:
+                tags = ['Sąd', keyword]
 
-def update_row(host, parent__name, name, **extra):
-    court = get_court(host, name)
-    parent_court = get_court(host, parent__name)
-    if not parent_court:
-        print(u"Unable to find parent court for {}".format(name))
-        return
-    url = urljoin(host, "/api/institutions/{}/".format(court))
-    params = {'parents_ids': [parent_court]}
+        data = {
+            "name": name,
+            "tags": tags,
+            "jst": normalize_jst(self._get_jst_id(data)),
+            "email": email,
+            "extra": {'db': extra, 'regon': data},
+            "regon": regon
+        }
+        response = self.s.post(url=urljoin(host, "/api/institutions/"), json=data)
+        if response.status_code == 500:
+            print(name.encode('utf-8'), " response 500", response.status_code, ":", file=sys.stderr)
+            print(response.text, file=sys.stderr)
+            return
+        json = response.json()
 
+        if response.status_code == 400 and json.keys() == ['regon']:
+            court = self._get_court(host, name)
+            parent_court = self._get_court(host, extra['parent__name'])
+            url = urljoin(host, "/api/institutions/{}/".format(court))
+            data['parents_ids'] = [parent_court]
+            self.s.patch(url, json=data)
+            if response.status_code == 500:
+                print(name.encode('utf-8'), " response 500", response.status_code, ":", file=sys.stderr)
+                print(response.text, file=sys.stderr)
+                return
 
-def fields_validation(fields):
-    result = True
-    for field_name in set(REQUIRED_FIELDS) - set(fields):
-        print("There is missing %s field. " % (field_name,) +
-              "Required fields name is {}".format(REQUIRED_FIELDS))
-        result = False
-    return result
+        if response.status_code == 201:
+            print(name.encode('utf-8'), " created as PK", json['pk'])
+        else:
+            print(name.encode('utf-8'), "response ", response.status_code, ":", file=sys.stderr)
+            print(json, file=sys.stderr)
 
+    def update_parent(self, host, parent__name, name, **extra):
+        court = self._get_court(host, name)
+        parent_court = self._get_court(host, parent__name)
+        if not parent_court:
+            print(u"Unable to find parent court for {}".format(name))
+            return
+        url = urljoin(host, "/api/institutions/{}/".format(court))
+        params = {'parents_ids': [parent_court]}
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input',
-                        required=True,
-                        type=argparse.FileType('r'),
-                        help="Input CSV-file")
-    parser.add_argument('--host',
-                        required=True,
-                        help="Host of instance")
-    parser.add_argument('--user',
-                        required=True,
-                        help="User to authentication")
-    parser.add_argument('--password',
-                        required=True,
-                        help="Password to authentication")
-    parser.add_argument('-t', '--tags',
-                        required=True,
-                        nargs='+')
-    args = parser.parse_args()
-    s.auth = (args.user, args.password)
+    def fields_validation(self, fields):
+        result = True
+        for field_name in set(self.REQUIRED_FIELDS) - set(fields):
+            print("There is missing %s field. " % (field_name,) +
+                  "Required fields name is {}".format(self.REQUIRED_FIELDS))
+            result = False
+        return result
 
-    reader = csv.DictReader(args.input)
+    def run(self):
+        reader = csv.DictReader(self.args.input)
 
-    if not fields_validation(reader.fieldnames):
-        print("Script stop, due previos erros.")
-        return
-    data = list(reader)
-    with trange(len(data) * 2, leave=True) as t:
-        for row in data:
-            t.set_description(row['name'])
-            t.update(1)
-        for row in data:
-            t.set_description(row['name'])
-            update_row(host=args.host, **row)
-            t.update(1)
+        if not self.fields_validation(reader.fieldnames):
+            print("Script stop, due previos erros.")
+            return
+        data = list(reader)
+        with trange(len(data) * 2, leave=True) as t:
+            for row in data:
+                self.insert_row(host=self.args.host, **row)
+                t.set_description(row['name'])
+                t.update(1)
+            for row in data:
+                t.set_description(row['name'])
+                self.update_parent(host=self.args.host, **row)
+                t.update(1)
+
 
 if __name__ == "__main__":
-    main()
+    Command(sys.argv).run()
