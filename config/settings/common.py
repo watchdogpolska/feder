c6fdeb9c1cb5b20e44e2cd4010b3f4c4704aad81
Jacek Lange <jacek.lange@gmail.com>
2017-09-02 14:22:11 +0200

    fixes #287 AttributeError: 'Settings' object has no attribute 'EMAIL_NOTIFICATION'

--- config/settings/common.py
+++ config/settings/common.py
@@ -326,6 +326,8 @@ SOCIALACCOUNT_PROVIDERS = {
     },
     'gilab': {'SCOPE': ['read_user', 'openid']}
 }
+EMAIL_NOTIFICATION = 'no-reply@siecobywatelska.pl'
+
 EMAILLABS_APP_KEY = env('EMAILLABS_APP_KEY', default="Dummy")
 
 EMAILLABS_SECRET_KEY = env('EMAILLABS_SECRET_KEY', default="Dummy")
