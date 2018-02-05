239e02fba96edf550f5b2c557b588019cfb510d3
Adam Dobrawy <naczelnik@jawnosc.tk>
2018-02-05 23:53:58 +0100

    Bump djangorestframework & django-filter version

--- config/settings/common.py
+++ config/settings/common.py
@@ -315,7 +315,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
 SOCIALACCOUNT_PROVIDERS = {
