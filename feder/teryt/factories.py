bcfaafdb59169f16af9b7fe9e703f1ad9708d6a8
Adam Dobrawy <naczelnik@jawnosc.tk>
2020-02-16 03:59:56 +0100

    Add filter for cases by JST & add tests for filter by JST

--- feder/teryt/factories.py
+++ feder/teryt/factories.py
@@ -1,8 +1,32 @@
+import factory
 from teryt_tree.factories import JednostkaAdministracyjnaFactory
 
-from feder.teryt import models
+from feder.teryt.models import JST
 
 
 class JSTFactory(JednostkaAdministracyjnaFactory):
     class Meta:
-        model = models.JST
+        model = JST
+
+    @factory.post_generation
+    def tree(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        JST.objects.rebuild()
+        self.refresh_from_db()
+
+
+class VoivodeshipJSTFactory(JednostkaAdministracyjnaFactory):
+    category__level = 1
+
+
+class CountyJSTFactory(JednostkaAdministracyjnaFactory):
+    category__level = 2
+    parent = factory.SubFactory(VoivodeshipJSTFactory)
+
+
+class CommunityJSTFactory(JednostkaAdministracyjnaFactory):
+    category__level = 3
+    parent = factory.SubFactory(CountyJSTFactory)
---
+++ feder/teryt/filters.py
@@ -0,0 +1,37 @@
+from dal import autocomplete
+
+from teryt_tree.dal_ext.filters import VoivodeshipFilter, CountyFilter, CommunityFilter
+from feder.main.mixins import DisabledWhenFilterMixin
+
+
+class DisabledWhenVoivodeshipFilter(DisabledWhenFilterMixin, VoivodeshipFilter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault(
+            "widget", autocomplete.ModelSelect2(url="teryt:voivodeship-autocomplete")
+        )
+        kwargs.setdefault("disabled_when", ["county", "community"])
+        super().__init__(*args, **kwargs)
+
+
+class DisabledWhenCountyFilter(DisabledWhenFilterMixin, CountyFilter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault(
+            "widget",
+            autocomplete.ModelSelect2(
+                url="teryt:county-autocomplete", forward=["voivodeship"]
+            ),
+        )
+        kwargs.setdefault("disabled_when", ["community"])
+        super().__init__(*args, **kwargs)
+
+
+class DisabledWhenCommunityFilter(DisabledWhenFilterMixin, CommunityFilter):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault(
+            "widget",
+            autocomplete.ModelSelect2(
+                url="teryt:community-autocomplete", forward=["county"]
+            ),
+        )
+        kwargs.setdefault("disabled_when", [])
+        super().__init__(*args, **kwargs)
