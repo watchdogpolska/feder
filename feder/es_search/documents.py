461bb1f998c5c2aa170e85c6235e85f1ec9f5255
Adam Dobrawy <ad-m@users.noreply.github.com>
2020-04-19 22:06:27 +0200

    Add term_vector for es (#727)

--- feder/es_search/documents.py
+++ feder/es_search/documents.py
@@ -2,9 +2,11 @@ from elasticsearch_dsl import Document, Keyword, Text
 
 
 class LetterDocument(Document):
-    title = Text(analyzer="snowball", required=True, fields={"raw": Keyword()})
-    body = Text(analyzer="snowball", required=True)
-    content = Text(analyzer="snowball", multi=True)
+    title = Text(
+        analyzer="snowball", required=True, fields={"raw": Keyword()}, term_vector="yes"
+    )
+    body = Text(analyzer="snowball", required=True, term_vector="yes")
+    content = Text(analyzer="snowball", multi=True, term_vector="yes")
     letter_id = Text(multi=False)
 
     class Index:
