d66b1ad439a7e1759f7f9aa7500f846938268af4
Kasia <efefre@icloud.com>
2020-10-07 22:46:08 +0200

    Add labels for county and community (MonitoringFilter)

--- feder/monitorings/filters.py
+++ feder/monitorings/filters.py
@@ -12,8 +12,8 @@ from feder.teryt.filters import (
 class MonitoringFilter(django_filters.FilterSet):
     created = django_filters.DateRangeFilter(label=_("Creation date"))
     voivodeship = DisabledWhenVoivodeshipFilter()
-    county = DisabledWhenCountyFilter()
-    community = DisabledWhenCommunityFilter()
+    county = DisabledWhenCountyFilter(label=_("County"))
+    community = DisabledWhenCommunityFilter(label=_("Community"))
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
