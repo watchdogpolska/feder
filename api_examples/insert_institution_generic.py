fb76c8b78878118bcf9521515b0568728270b3e0
dzejkob <messer>
2020-10-18 20:22:09 +0200

    Generic script for uploading institutions form csv file.

--- api_examples/insert_institution_generic.py
+++ api_examples/insert_institution_generic.py
@@ -38,13 +38,16 @@ class Command:
     REQUIRED_FIELDS = ["name", "email", "tags"]
 
     def __init__(self, argv):
-        self.gus = GUS(
-            api_key=environ("GUSREGON_API_KEY", required=False),
-            sandbox=environ("GUSREGON_SANDBOX", True),
-        )
-        self.s = requests.Session()
         self.argv = argv
         self.args = self.get_build_args(argv[1:])
+        self.gus = GUS(
+            api_key=(
+                self.args.gusregon_api_key
+                or environ("GUSREGON_API_KEY", required=False)
+            ),
+            sandbox=self.args.gusregon_sandbox,
+        )
+        self.s = requests.Session()
         self.s.auth = (self.args.user, self.args.password)
         self.done_count = 0
         self.total_count = None
@@ -60,10 +63,60 @@ class Command:
             "--password", required=True, help="Password to authentication"
         )
         parser.add_argument(
+            "--gusregon-sandbox",
+            required=False,
+            help="GUS REGON sandbox mode (no API key needed)",
+            dest="gusregon_sandbox",
+            action="store_true",
+            default=False,
+        )
+        parser.add_argument(
+            "--gusregon-api-key",
+            required=False,
+            help="GUS REGON API KEY",
+            dest="gusregon_api_key",
+            action="store",
+            default=None,
+        )
+        parser.add_argument(
+            "--name-exact",
+            required=False,
+            help="Do institution name should match exact GUS data name?",
+            dest="name_exact",
+            action="store_true",
+            default=False,
+        )
+        parser.add_argument(
+            "--name-prefix",
+            required=False,
+            help="Prefix of the institution name which is expected to be the same",
+            dest="name_prefix",
+            action="store",
+            default="",
+        )
+        parser.add_argument(
+            "--name-suffix-field",
+            required=False,
+            help="In what field of GUS REGON data should institution suffix "
+            "be looked for",
+            dest="name_suffix_field",
+            action="store",
+            default=None,
+        )
+        parser.add_argument(
             "--simulate",
             required=False,
             help="Do not insert any data",
             dest="simulate",
+            action="store_true",
+            default=False,
+        )
+        parser.add_argument(
+            "--explicit",
+            required=False,
+            help="Prints detailed data of the institution if GUS data "
+            "validation fails.",
+            dest="explicit",
             action="store_true",
             default=False,
         )
@@ -83,16 +136,72 @@ class Command:
             ipdb.set_trace()
         return data["results"][0]["pk"]
 
+    def _print_gus_data(self, name, gus_data):
+        print('GUS data for institution "{}":'.format(name))
+        for key, val in gus_data.items():
+            if val:
+                print(" - {}: {}".format(key, val))
+        print()
+
     def insert_row(  # noqa: C901
         self, host, name, email, tags, regon=None, regon_parent=None, **extra
     ):
+        data = {"name": name, "tags": tags.split(","), "email": email}
+
         regon_data = None
         if regon:
-            regon = regon.strip(">").strip("*").strip()
+            # REGON normalization
+            regon = regon.strip(".").strip().rjust(9, "0")
             regon_data = self.gus.search(regon=regon)
+
             if not regon_data:
-                raise Exception("Invalid regon data", regon, name)
-        data = {"name": name, "tags": tags.split(","), "email": email}
+                raise Exception("Invalid REGON data", regon, name)
+
+            regon_data_name = regon_data.get("nazwa", "?").upper()
+
+            if self.args.name_exact and name.upper() != regon_data_name:
+                print(
+                    'Warning! GUS name "{}" does not match '
+                    'institution name "{}"'.format(regon_data_name, name.upper())
+                )
+                if self.args.explicit:
+                    self._print_gus_data(name, regon_data)
+
+            if (
+                self.args.name_prefix
+                and self.args.name_prefix.upper() not in regon_data_name
+            ):
+                print(
+                    'Warning! GUS name "{}" does not contain '
+                    'expected prefix "{}".'.format(
+                        regon_data_name, self.args.name_prefix.upper()
+                    )
+                )
+                if self.args.explicit:
+                    self._print_gus_data(name, regon_data)
+
+            # this is dynamic name part after removing constant prefix
+            expected_suffix = (
+                name.upper().replace(self.args.name_prefix.upper(), "").strip()
+            )
+            regon_suffix = regon_data.get(self.args.name_suffix_field, "").upper()
+
+            if self.args.name_suffix_field and expected_suffix not in regon_suffix:
+                print(
+                    'Warning! GUS field "{}" does not contain '
+                    'expected value "{}" but "{}".'.format(
+                        self.args.name_suffix_field, expected_suffix, regon_suffix
+                    )
+                )
+                if self.args.explicit:
+                    self._print_gus_data(name, regon_data)
+
+        else:
+            raise Exception(
+                'Regon value is missing in input file for institution: "{}".'.format(
+                    name
+                )
+            )
 
         if regon:
             data.update({"regon": regon})
@@ -133,28 +242,27 @@ class Command:
                         json=data,
                     )
                 else:
-                    print("Simulated PATCH for {}".format(name.encode("utf-8")))
+                    print("Simulated PATCH for {}\n".format(name))
             else:
                 if not self.args.simulate:
                     response = self.s.post(
                         url=urljoin(host, "/api/institutions/"), json=data
                     )
                 else:
-                    print("Simulated POST for {}".format(name.encode("utf-8")))
+                    print("Simulated POST for {}\n".format(name))
         else:
             if not self.args.simulate:
                 response = self.s.post(
                     url=urljoin(host, "/api/institutions/"), json=data
                 )
             else:
-                print("Simulated POST for {}".format(name.encode("utf-8")))
+                print("Simulated POST for {}\n".format(name))
 
         if response and response.status_code >= 300:
             print(
-                name.encode("utf-8"),
-                " response 500",
-                response.status_code,
-                ":",
+                'Institution: "{}"; response status: {}.\n'.format(
+                    name, response.status_code
+                ),
                 file=sys.stderr,
             )
             print(response.text, file=sys.stderr)
@@ -166,13 +274,13 @@ class Command:
         if response:
             json = response.json()
             if response.status_code == 201:
-                print(progress, name.encode("utf-8"), " created as PK", json["pk"])
+                print(progress, name, " created as PK", json["pk"])
             elif response.status_code == 200:
-                print(progress, name.encode("utf-8"), " updated as PK", json["pk"])
+                print(progress, name, " updated as PK", json["pk"])
             else:
                 print(
                     progress,
-                    name.encode("utf-8"),
+                    name,
                     "response ",
                     response.status_code,
                     ":",
