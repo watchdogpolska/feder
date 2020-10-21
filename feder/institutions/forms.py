240283be9fb54e611d3b175b1f1f0645275c1652
dzejkob <messer>
2020-10-21 12:21:03 +0200

    Simpler form layout definition.

--- feder/institutions/forms.py
+++ feder/institutions/forms.py
@@ -1,22 +1,18 @@
+from atom.ext.crispy_forms.forms import SingleButtonMixin
 from braces.forms import UserKwargModelFormMixin
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Layout, Fieldset
 from dal import autocomplete
 from django import forms
-from django.utils.translation import ugettext_lazy as _
 
 from .models import Institution
 
 
-class InstitutionForm(UserKwargModelFormMixin, forms.ModelForm):
+class InstitutionForm(SingleButtonMixin, UserKwargModelFormMixin, forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Form "helper" is needed to have control over rendering by "crispy" tag.
-        self.helper = FormHelper()
         self.helper.layout = Layout(
             Fieldset("", "name", "jst", "regon", "email"),
             Fieldset("", "tags", css_class="form-group scrollable-widget"),
-            ButtonHolder(Submit("submit", _("Update"), css_class="btn btn-primary")),
         )
 
     class Meta:
