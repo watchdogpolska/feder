d39f3522eea76acb5d84fd1f85ab0730d7d84094
dzejkobi <jakubstanclik@gmail.com>
2020-10-21 13:02:32 +0200

    Changed tag selection widget to checkboxes in institution edit page. (#868)
    * Changed tag selection widget to checkboxes in institution edit page.
    
    * Simpler form layout definition.
    
    Co-authored-by: dzejkob <messer>

--- feder/institutions/forms.py
+++ feder/institutions/forms.py
@@ -1,5 +1,6 @@
 from atom.ext.crispy_forms.forms import SingleButtonMixin
 from braces.forms import UserKwargModelFormMixin
+from crispy_forms.layout import Layout, Fieldset
 from dal import autocomplete
 from django import forms
 
@@ -7,7 +8,17 @@ from .models import Institution
 
 
 class InstitutionForm(SingleButtonMixin, UserKwargModelFormMixin, forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper.layout = Layout(
+            Fieldset("", "name", "jst", "regon", "email"),
+            Fieldset("", "tags", css_class="form-group scrollable-widget"),
+        )
+
     class Meta:
         model = Institution
         fields = ["name", "tags", "jst", "regon", "email"]
-        widgets = {"jst": autocomplete.ModelSelect2(url="teryt:community-autocomplete")}
+        widgets = {
+            "jst": autocomplete.ModelSelect2(url="teryt:community-autocomplete"),
+            "tags": forms.CheckboxSelectMultiple,
+        }
