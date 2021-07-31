81dc955c66bdb0c401078fd878c53e221f5b8c77
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-07-31 23:39:57 +0200

    Add model for organisation

---
+++ feder/organisations/models.py
@@ -0,0 +1,26 @@
+from autoslug.fields import AutoSlugField
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from model_utils.models import TimeStampedModel
+
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class OrganisationQuerySet(models.QuerySet):
+    pass
+
+class Organisation(TimeStampedModel):
+    name = models.CharField(verbose_name=_("Name"), max_length=100)
+    slug = AutoSlugField(
+        populate_from="name", max_length=110, verbose_name=_("Slug"), unique=True
+    )
+    objects = OrganisationQuerySet.as_manager()
+
+    class Meta:
+        verbose_name = _("Organisation")
+        verbose_name_plural = _("Organisations")
+        ordering = ["created"]
+
+    def __str__(self):
+        return str(self.name)
