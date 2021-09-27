ab342deff8aab3258935d28849d767071c00a5c2
Adam Dobrawy <naczelnik@jawnosc.tk>
2021-09-28 00:55:54 +0200

    Fix AppConfig to fix rtfd build

--- feder/domains/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-from django.utils.translation import ugettext_lazy as _
-
-
-class DomainsConfig(AppConfig):
-    name = "feder.domains"
-    verbose_name = _("Domains")
--- feder/organisations/apps.py
+++ feder/organisations/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class OrganisationsConfig(AppConfig):
-    name = "organisations"
+    name = "feder.organisations"
