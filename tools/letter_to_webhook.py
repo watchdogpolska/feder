09ce77d92432adec55323662c39fa94372cdd859
PiotrIw <piotr.iwanski@gmail.com>
2023-02-09 18:45:28 +0000

    feat: letter_to_webhook helper finalised

--- tools/letter_to_webhook.py
+++ tools/letter_to_webhook.py
@@ -1,57 +1,57 @@
 import os
 import sys
-sys.path.insert(0, '/code/tools')
 import time
 import json
-print(os. getcwd())
-from mail_parser import serialize_mail, get_message, get_manifest
-
 import requests
 import mailparser
+import argparse
 
+tools_dir = os.path.dirname(__file__)
+sys.path.insert(0, tools_dir)
 
-webhook = 'http://localhost:8000/listy/webhook?secret=my-strong-secret'
+from mail_parser import serialize_mail, get_message, get_manifest
+
+# print(tools_dir)
+
+description = """
+Send email from eml file to feder webhook for registration or webhook test.
+"""
+url_help = """
+webhook url example: http://localhost:8000/listy/webhook?secret=my-strong-secret 
+"""
+file_help = """
+file example: /code/feder/media_prod/messages/2022/04/03/c6ceae935fed401bae7d12da3be6c775.eml.gz
+"""
+
 def main():
-    # total arguments
-    n = len(sys.argv)
-    print("Total arguments passed:", n)
-    
-    # Arguments passed
-    print("\nName of Python script:", sys.argv[0])
-    
-    print("\nArguments passed:", end = "\n")
-    for i in range(1, n):
-        print(sys.argv[i], end = "\n")
-    msg = get_message(sys.argv[i])
-    process_msg(msg)
-    # config = get_config(os.environ)
-    # session = requests.Session()
-    # print("Configuration: ", config)
-    # if config["sentry_dsn"]:
-    #     try:
-    #         loop(config, session)
-    #     except Exception as e:
-    #         sentry_sdk.capture_exception(e)
-    # else:
-    #     loop(config, session, None)
+    parser = argparse.ArgumentParser(description=description, add_help=True)
+    parser.add_argument("--url", help=url_help, required=True)
+    parser.add_argument("--file", help=file_help, required=True)
+    try:
+        args = vars(parser.parse_args())
+    except argparse.ArgumentError:
+        parser.print_help()
+    print(args)
+    process_msg(msg_file=args["file"], webhook_url=args["url"])
 
 
-def process_msg(msg):
-    print(msg)
-    mail = mailparser.parse_from_bytes(msg)
+def process_msg(msg_file, webhook_url):
+    print(f"Eml file to processs: {msg_file}")
+    msg_content = get_message(msg_file)
+    mail = mailparser.parse_from_bytes(msg_content)
     body = get_manifest(mail, False)
-    json.dump(body, sys.stdout, indent=4)
+    # json.dump(body, sys.stdout, indent=4)
     start = time.time()
-    email_body = serialize_mail(msg, compress_eml=True)
+    email_body = serialize_mail(msg_content, compress_eml=True)
     msg_headers = body.get("headers") or {}
     msg_id = msg_headers.get("message_id")
     end = time.time()
-    print("Message serialized in {} seconds".format(end - start))
-    res = requests.post(webhook, files=email_body)
+    print(f"Message {msg_id} serialized in {end - start} seconds")
+    res = requests.post(webhook_url, files=email_body)
     print("Received response:", res.text)
     res.raise_for_status()
-    response = res.json()
-
+    response_txt = json.dumps(res.json(), indent=2)
+    print("Full response: ", response_txt, "\n")
 
 
 if __name__ == "__main__":
