b1c21c8cc82c44795d27aada8151bbf9bc5ba39f
PiotrIw <piotr.iwanski@gmail.com>
2023-10-19 16:33:59 +0000

    feat: initiate langchain letter promprt

---
+++ feder/llm_evaluation/prompts.py
@@ -0,0 +1,50 @@
+from langchain.prompts import PromptTemplate
+
+
+letter_evaluation_instruction = PromptTemplate.from_template(
+    """
+    W odpowiedzi na wniosek o informację publiczną do {institution}, o treści:
+    ```{monitoring_question}```,
+    otrzymano email z załącznikami o treści:
+    ```{response}```.
+    """
+)
+
+letter_categorization = PromptTemplate.from_template(
+    """{instruction}
+    Oceń otrzymany email z załącznikami i przypisz go do jednej z kategorii,
+    z listy poniżej wybierając odpowiednią literę wraz z opisem:
+    ```
+    A) email jest odpowiedzią z {institution} i zawiera odpowiedzi na pytania z
+        wniosku o informację publiczną.
+    B) email jest odpowiedzią z {institution} i zawiera odmowę odpowiedzi na
+        pytania z wniosku o informację publiczną.
+    C) email jest odpowiedzią z {institution} i zawiera informację o
+        przedłużeniu terminu na odpowiedź.
+    D) email jest potwierdzeniem dostarczenia lub otwarcia maila z {institution}
+        i nie zawiera odpowiedzi na pytania z wniosku o informację publiczną.
+    E) email jest odpowiedzią z innej instytucji lub na inny wniosek.
+    F) email nie jest odpowiedzią z {institution} i jest spamem.
+    G) nie można ustalić kategorii odpowiedzi.
+    ```
+    """,
+)
+
+letter_response_answers_evaluation = PromptTemplate.from_template(
+    """{instruction}
+    Oceń otrzymany email z załącznikami i podaj oznaczenia pytań z wniosku o
+    informację publiczną bez ich treści, na które udzielono odpowiedzi oraz
+    numery pytań z wniosku na które odmówiono udzielenia odpowiedzi. Osobno
+    podaj oznaczenia pytań dla których poproszono o wydłużenie terminu na
+    odpowiedź.
+    """,
+)
+
+letter_response_formatting = PromptTemplate.from_template(
+    """{instruction}
+    Zbuduj ujednoliconą formę dokumentu informacyjnego zawierającego listę
+    pytań i odpowiedzi w postaci:
+    Pytanie: "pytanie z wniosku o informację publiczną"
+    Odpowiedź: "odpowiedź z maila"
+    """,
+)
