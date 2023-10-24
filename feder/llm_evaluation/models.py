266e8fe7b529edc103cbcb3ac554bf0fcf1f835e
PiotrIw <piotr.iwanski@gmail.com>
2023-10-24 11:53:03 +0000

    feat: add categorize_letter class method

--- feder/llm_evaluation/models.py
+++ feder/llm_evaluation/models.py
@@ -1,8 +1,18 @@
+import time
+
+from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from jsonfield import JSONField
+from langchain.callbacks import get_openai_callback
+from langchain.chat_models import ChatOpenAI
+from langchain.schema.output_parser import StrOutputParser
+from langchain.text_splitter import TokenTextSplitter
 from model_utils import Choices
 from model_utils.models import TimeStampedModel
+
+from .llm_tools import num_tokens_from_string
+from .prompts import letter_categorization, letter_evaluation_intro
 
 
 class LLmRequestQuerySet(models.QuerySet):
@@ -42,6 +52,79 @@ class LlmLetterRequest(LlmRequest):
         on_delete=models.DO_NOTHING,
         verbose_name=_("Evaluated Letter"),
     )
+
+    @classmethod
+    def categorize_letter(cls, letter):
+        intro = letter_evaluation_intro.format(
+            institution=letter.case.institution.name,
+            monitoring_question=letter.case.monitoring.template,
+        )
+        test_prompt = letter_categorization.format(
+            intro=intro,
+            institution=letter.case.institution.name,
+            monitoring_response="",
+        )
+        q_tokens = num_tokens_from_string(
+            test_prompt, settings.OPENAI_API_ENGINE.replace("so-", "")
+        )
+        # print(f"q_tokens: {q_tokens}")
+        max_tokens = 8000 - q_tokens - 500
+        # print(f"max_tokens: {max_tokens}")
+        text_splitter = TokenTextSplitter(chunk_size=max_tokens, chunk_overlap=100)
+        texts = text_splitter.split_text(letter.get_full_content())
+        # print(
+        #     "texts[0] tokens:",
+        #     num_tokens_from_string(
+        #         texts[0], settings.OPENAI_API_ENGINE.replace("so-", "")
+        #     ),
+        # )
+        final_prompt = letter_categorization.format(
+            intro=intro,
+            institution=letter.case.institution.name,
+            monitoring_response=texts[0],
+        )
+        letter_llm_request = cls.objects.create(
+            evaluated_letter=letter,
+            engine_name=settings.OPENAI_API_ENGINE,
+            request_prompt=final_prompt,
+            status=cls.STATUS.created,
+            response="",
+            token_usage={},
+        )
+        letter_llm_request.save()
+        model = ChatOpenAI(
+            model_kwargs={
+                "api_type": settings.OPENAI_API_TYPE,
+                "api_key": settings.OPENAI_API_KEY,
+                "api_version": settings.OPENAI_API_VERSION,
+                "api_base": settings.OPENAI_API_BASE,
+                "engine": settings.OPENAI_API_ENGINE,
+            },
+            temperature=settings.OPENAI_LLM_TEMPERATURE,
+        )
+        chain = letter_categorization | model | StrOutputParser()
+        start_time = time.time()
+        with get_openai_callback() as cb:
+            resp = chain.invoke(
+                {
+                    "intro": intro,
+                    "institution": letter.case.institution.name,
+                    "monitoring_response": texts[0],
+                }
+            )
+        end_time = time.time()
+        execution_time = end_time - start_time
+        llm_info_dict = vars(cb)
+        llm_info_dict["completion_time"] = execution_time
+        letter_llm_request.response = resp
+        letter_llm_request.token_usage = llm_info_dict
+        letter_llm_request.status = cls.STATUS.done
+        letter_llm_request.save()
+        letter.ai_evaluation = resp
+        letter.save()
+        # print(f"resp: {resp}")
+        # print(f"cb: {cb}")
+        # print(f"execution_time: {execution_time}")
 
 
 class LlmMonitoringRequest(LlmRequest):
