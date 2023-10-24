421a8b3a0c5688729ec66b0e7e718c6d9ec1db33
PiotrIw <piotr.iwanski@gmail.com>
2023-10-24 16:59:29 +0000

    refactor: less repetitions and better readability

--- feder/llm_evaluation/tasks.py
+++ feder/llm_evaluation/tasks.py
@@ -12,18 +12,20 @@ def categorize_letter_in_background(letter_pk):
     from feder.letters.models import Letter
 
     letter = Letter.objects.filter(pk=letter_pk).first()
-    if (
-        letter
-        and letter.is_incoming
-        and letter.is_spam not in [Letter.SPAM.spam, Letter.SPAM.probable_spam]
-    ):
-        LlmLetterRequest.categorize_letter(letter)
-        logger.info(f"Letter with pk={letter_pk} categorized.")
-    elif letter and not letter.is_incoming:
+
+    if not letter:
+        logger.warning(f"Letter with pk={letter_pk} not found.")
+        return
+
+    if not letter.is_incoming:
         logger.warning(
             f"Letter (pk={letter_pk}) categorisation skipped - letter is not incoming."
         )
-    elif letter and letter.is_spam in [Letter.SPAM.spam, Letter.SPAM.probable_spam]:
+        return
+
+    if letter.is_spam in [Letter.SPAM.spam, Letter.SPAM.probable_spam]:
         logger.warning(f"Letter (pk={letter_pk}) is spam, categorisation skipped.")
-    else:
-        logger.warning(f"Letter with pk={letter_pk} not found.")
+        return
+
+    LlmLetterRequest.categorize_letter(letter)
+    logger.info(f"Letter with pk={letter_pk} categorized.")
