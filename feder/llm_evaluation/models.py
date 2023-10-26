fcf8e6fc5e33b0441473018867e41f8b35770eed
PiotrIw <piotr.iwanski@gmail.com>
2023-10-26 16:11:55 +0000

    fix: add correct cost calculation

--- feder/llm_evaluation/models.py
+++ feder/llm_evaluation/models.py
@@ -49,6 +49,91 @@ class LlmRequest(TimeStampedModel):
     class Meta:
         abstract = True
 
+    def update_usage_cost(self):
+        """
+        Calculate and update cost of LLM request based on number of tokens used
+        and cost per 1000 tokens for given model using info in format:
+        {
+            "prompt_tokens": 821,
+            "completion_tokens": 245,
+        }
+
+        """
+        MODEL_COST_PER_1K_TOKENS = {
+            # GPT-4 input
+            "gpt-4": 0.03,
+            "gpt-4-0314": 0.03,
+            "gpt-4-0613": 0.03,
+            "gpt-4-32k": 0.06,
+            "gpt-4-32k-0314": 0.06,
+            "gpt-4-32k-0613": 0.06,
+            # GPT-4 output
+            "gpt-4-completion": 0.06,
+            "gpt-4-0314-completion": 0.06,
+            "gpt-4-0613-completion": 0.06,
+            "gpt-4-32k-completion": 0.12,
+            "gpt-4-32k-0314-completion": 0.12,
+            "gpt-4-32k-0613-completion": 0.12,
+            # GPT-3.5 input
+            "gpt-3.5-turbo": 0.0015,
+            "gpt-3.5-turbo-0301": 0.0015,
+            "gpt-3.5-turbo-0613": 0.0015,
+            "gpt-3.5-turbo-instruct": 0.0015,
+            "gpt-3.5-turbo-16k": 0.003,
+            "gpt-3.5-turbo-16k-0613": 0.003,
+            # GPT-3.5 output
+            "gpt-3.5-turbo-completion": 0.002,
+            "gpt-3.5-turbo-0301-completion": 0.002,
+            "gpt-3.5-turbo-0613-completion": 0.002,
+            "gpt-3.5-turbo-instruct-completion": 0.002,
+            "gpt-3.5-turbo-16k-completion": 0.004,
+            "gpt-3.5-turbo-16k-0613-completion": 0.004,
+            # Azure GPT-35 input
+            "gpt-35-turbo": 0.0015,  # Azure OpenAI version of ChatGPT
+            "gpt-35-turbo-0301": 0.0015,  # Azure OpenAI version of ChatGPT
+            "gpt-35-turbo-0613": 0.0015,
+            "gpt-35-turbo-instruct": 0.0015,
+            "gpt-35-turbo-16k": 0.003,
+            "gpt-35-turbo-16k-0613": 0.003,
+            # Azure GPT-35 output
+            "gpt-35-turbo-completion": 0.002,  # Azure OpenAI version of ChatGPT
+            "gpt-35-turbo-0301-completion": 0.002,  # Azure OpenAI version of ChatGPT
+            "gpt-35-turbo-0613-completion": 0.002,
+            "gpt-35-turbo-instruct-completion": 0.002,
+            "gpt-35-turbo-16k-completion": 0.004,
+            "gpt-35-turbo-16k-0613-completion": 0.004,
+        }
+
+        if self.token_usage:
+            standarised_prompt_name = [
+                name
+                for name in MODEL_COST_PER_1K_TOKENS.keys()
+                if name.startswith(self.engine_name) and not name.endswith("completion")
+            ][-1]
+            standarised_completion_name = [
+                name
+                for name in MODEL_COST_PER_1K_TOKENS.keys()
+                if name.startswith(self.engine_name) and name.endswith("completion")
+            ][-1]
+            print(
+                self.engine_name, standarised_prompt_name, standarised_completion_name
+            )
+            if standarised_prompt_name and standarised_completion_name:
+                prompt_cost = (
+                    self.token_usage["prompt_tokens"]
+                    / 1000
+                    * MODEL_COST_PER_1K_TOKENS[standarised_prompt_name]
+                )
+                completion_cost = (
+                    self.token_usage["completion_tokens"]
+                    / 1000
+                    * MODEL_COST_PER_1K_TOKENS[standarised_completion_name]
+                )
+                self.token_usage["calculated_usage_cost"] = (
+                    prompt_cost + completion_cost
+                )
+                self.save()
+
 
 class LlmLetterRequest(LlmRequest):
     evaluated_letter = models.ForeignKey(
@@ -77,7 +162,7 @@ class LlmLetterRequest(LlmRequest):
             test_prompt, settings.OPENAI_API_ENGINE.replace("so-", "")
         )
         # print(f"q_tokens: {q_tokens}")
-        max_tokens = 8000 - q_tokens - 500
+        max_tokens = settings.OPENAI_API_ENGINE_MAX_TOKENS - q_tokens - 500
         # print(f"max_tokens: {max_tokens}")
         text_splitter = TokenTextSplitter(chunk_size=max_tokens, chunk_overlap=100)
         texts = text_splitter.split_text(letter.get_full_content())
@@ -129,6 +214,7 @@ class LlmLetterRequest(LlmRequest):
         letter_llm_request.token_usage = llm_info_dict
         letter_llm_request.status = cls.STATUS.done
         letter_llm_request.save()
+        letter_llm_request.update_usage_cost()
         letter.ai_evaluation = resp
         letter.save()
         # print(f"resp: {resp}")
