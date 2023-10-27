c0e520ce8b9ac652d68446a9478e1bb991278022
PiotrIw <piotr.iwanski@gmail.com>
2023-10-27 12:07:47 +0000

    fix: apply proper azure model

--- feder/llm_evaluation/llm_tools.py
+++ feder/llm_evaluation/llm_tools.py
@@ -4,7 +4,7 @@ import time
 import tiktoken
 from django.conf import settings
 from langchain.callbacks import get_openai_callback
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
 
 logger = logging.getLogger(__name__)
@@ -18,14 +18,12 @@ def num_tokens_from_string(string: str, model: str) -> int:
 
 
 def get_llm_response(prompt, prompt_kwargs_dict):
-    model = ChatOpenAI(
-        model_kwargs={
-            "api_type": settings.OPENAI_API_TYPE,
-            "api_key": settings.OPENAI_API_KEY,
-            "api_version": settings.OPENAI_API_VERSION,
-            "api_base": settings.OPENAI_API_BASE,
-            "engine": settings.OPENAI_API_ENGINE,
-        },
+    model = AzureChatOpenAI(
+        openai_api_type=settings.OPENAI_API_TYPE,
+        openai_api_key=settings.OPENAI_API_KEY,
+        openai_api_version=settings.OPENAI_API_VERSION,
+        openai_api_base=settings.OPENAI_API_BASE,
+        deployment_name=settings.OPENAI_API_DEPLOYMENT_NAME,
         temperature=settings.OPENAI_API_TEMPERATURE,
     )
     chain = prompt | model | StrOutputParser()
--- feder/llm_evaluation/models.py
+++ feder/llm_evaluation/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from jsonfield import JSONField
 from langchain.callbacks import get_openai_callback
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import AzureChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
 from langchain.text_splitter import TokenTextSplitter
 from model_utils import Choices
@@ -49,91 +49,6 @@ class LlmRequest(TimeStampedModel):
     class Meta:
         abstract = True
 
-    def update_usage_cost(self):
-        """
-        Calculate and update cost of LLM request based on number of tokens used
-        and cost per 1000 tokens for given model using info in format:
-        {
-            "prompt_tokens": 821,
-            "completion_tokens": 245,
-        }
-
-        """
-        MODEL_COST_PER_1K_TOKENS = {
-            # GPT-4 input
-            "gpt-4": 0.03,
-            "gpt-4-0314": 0.03,
-            "gpt-4-0613": 0.03,
-            "gpt-4-32k": 0.06,
-            "gpt-4-32k-0314": 0.06,
-            "gpt-4-32k-0613": 0.06,
-            # GPT-4 output
-            "gpt-4-completion": 0.06,
-            "gpt-4-0314-completion": 0.06,
-            "gpt-4-0613-completion": 0.06,
-            "gpt-4-32k-completion": 0.12,
-            "gpt-4-32k-0314-completion": 0.12,
-            "gpt-4-32k-0613-completion": 0.12,
-            # GPT-3.5 input
-            "gpt-3.5-turbo": 0.0015,
-            "gpt-3.5-turbo-0301": 0.0015,
-            "gpt-3.5-turbo-0613": 0.0015,
-            "gpt-3.5-turbo-instruct": 0.0015,
-            "gpt-3.5-turbo-16k": 0.003,
-            "gpt-3.5-turbo-16k-0613": 0.003,
-            # GPT-3.5 output
-            "gpt-3.5-turbo-completion": 0.002,
-            "gpt-3.5-turbo-0301-completion": 0.002,
-            "gpt-3.5-turbo-0613-completion": 0.002,
-            "gpt-3.5-turbo-instruct-completion": 0.002,
-            "gpt-3.5-turbo-16k-completion": 0.004,
-            "gpt-3.5-turbo-16k-0613-completion": 0.004,
-            # Azure GPT-35 input
-            "gpt-35-turbo": 0.0015,  # Azure OpenAI version of ChatGPT
-            "gpt-35-turbo-0301": 0.0015,  # Azure OpenAI version of ChatGPT
-            "gpt-35-turbo-0613": 0.0015,
-            "gpt-35-turbo-instruct": 0.0015,
-            "gpt-35-turbo-16k": 0.003,
-            "gpt-35-turbo-16k-0613": 0.003,
-            # Azure GPT-35 output
-            "gpt-35-turbo-completion": 0.002,  # Azure OpenAI version of ChatGPT
-            "gpt-35-turbo-0301-completion": 0.002,  # Azure OpenAI version of ChatGPT
-            "gpt-35-turbo-0613-completion": 0.002,
-            "gpt-35-turbo-instruct-completion": 0.002,
-            "gpt-35-turbo-16k-completion": 0.004,
-            "gpt-35-turbo-16k-0613-completion": 0.004,
-        }
-
-        if self.token_usage:
-            standarised_prompt_name = [
-                name
-                for name in MODEL_COST_PER_1K_TOKENS.keys()
-                if name.startswith(self.engine_name) and not name.endswith("completion")
-            ][-1]
-            standarised_completion_name = [
-                name
-                for name in MODEL_COST_PER_1K_TOKENS.keys()
-                if name.startswith(self.engine_name) and name.endswith("completion")
-            ][-1]
-            print(
-                self.engine_name, standarised_prompt_name, standarised_completion_name
-            )
-            if standarised_prompt_name and standarised_completion_name:
-                prompt_cost = (
-                    self.token_usage["prompt_tokens"]
-                    / 1000
-                    * MODEL_COST_PER_1K_TOKENS[standarised_prompt_name]
-                )
-                completion_cost = (
-                    self.token_usage["completion_tokens"]
-                    / 1000
-                    * MODEL_COST_PER_1K_TOKENS[standarised_completion_name]
-                )
-                self.token_usage["calculated_usage_cost"] = (
-                    prompt_cost + completion_cost
-                )
-                self.save()
-
 
 class LlmLetterRequest(LlmRequest):
     evaluated_letter = models.ForeignKey(
@@ -162,7 +77,7 @@ class LlmLetterRequest(LlmRequest):
             test_prompt, settings.OPENAI_API_ENGINE.replace("so-", "")
         )
         # print(f"q_tokens: {q_tokens}")
-        max_tokens = settings.OPENAI_API_ENGINE_MAX_TOKENS - q_tokens - 500
+        max_tokens = min(settings.OPENAI_API_ENGINE_MAX_TOKENS, 6000) - q_tokens - 500
         # print(f"max_tokens: {max_tokens}")
         text_splitter = TokenTextSplitter(chunk_size=max_tokens, chunk_overlap=100)
         texts = text_splitter.split_text(letter.get_full_content())
@@ -177,23 +92,22 @@ class LlmLetterRequest(LlmRequest):
             institution=institution_name,
             monitoring_response=texts[0],
         )
+        llm_engine = settings.OPENAI_API_ENGINE
         letter_llm_request = cls.objects.create(
             evaluated_letter=letter,
-            engine_name=settings.OPENAI_API_ENGINE,
+            engine_name=llm_engine,
             request_prompt=final_prompt,
             status=cls.STATUS.created,
             response="",
             token_usage={},
         )
         letter_llm_request.save()
-        model = ChatOpenAI(
-            model_kwargs={
-                "api_type": settings.OPENAI_API_TYPE,
-                "api_key": settings.OPENAI_API_KEY,
-                "api_version": settings.OPENAI_API_VERSION,
-                "api_base": settings.OPENAI_API_BASE,
-                "engine": settings.OPENAI_API_ENGINE,
-            },
+        model = AzureChatOpenAI(
+            openai_api_type=settings.OPENAI_API_TYPE,
+            openai_api_key=settings.OPENAI_API_KEY,
+            openai_api_version=settings.OPENAI_API_VERSION,
+            openai_api_base=settings.OPENAI_API_BASE,
+            deployment_name=llm_engine,
             temperature=settings.OPENAI_API_TEMPERATURE,
         )
         chain = letter_categorization | model | StrOutputParser()
@@ -214,7 +128,6 @@ class LlmLetterRequest(LlmRequest):
         letter_llm_request.token_usage = llm_info_dict
         letter_llm_request.status = cls.STATUS.done
         letter_llm_request.save()
-        letter_llm_request.update_usage_cost()
         letter.ai_evaluation = resp
         letter.save()
         # print(f"resp: {resp}")
@@ -244,14 +157,12 @@ class LlmMonitoringRequest(LlmRequest):
             token_usage={},
         )
         monitoring_llm_request.save()
-        model = ChatOpenAI(
-            model_kwargs={
-                "api_type": settings.OPENAI_API_TYPE,
-                "api_key": settings.OPENAI_API_KEY,
-                "api_version": settings.OPENAI_API_VERSION,
-                "api_base": settings.OPENAI_API_BASE,
-                "engine": llm_engine,
-            },
+        model = AzureChatOpenAI(
+            openai_api_type=settings.OPENAI_API_TYPE,
+            openai_api_key=settings.OPENAI_API_KEY,
+            openai_api_version=settings.OPENAI_API_VERSION,
+            openai_api_base=settings.OPENAI_API_BASE,
+            deployment_name=llm_engine,
             temperature=settings.OPENAI_API_TEMPERATURE,
         )
         chain = monitoring_response_normalized_template | model | StrOutputParser()
@@ -266,6 +177,5 @@ class LlmMonitoringRequest(LlmRequest):
         monitoring_llm_request.token_usage = llm_info_dict
         monitoring_llm_request.status = cls.STATUS.done
         monitoring_llm_request.save()
-        monitoring_llm_request.update_usage_cost()
         monitoring.normalized_response_template = resp
         monitoring.save()
