d187ef6f43e0a8047cf27b0e0e83cd9eca19ac78
PiotrIw <piotr.iwanski@gmail.com>
2023-10-27 11:48:43 +0000

    fix: avoid error for Null values

--- feder/letters/models.py
+++ feder/letters/models.py
@@ -488,6 +488,7 @@ class Letter(AbstractRecord):
         attachments_text_content_list = [
             attachment.text_content
             if attachment.text_content_update_result == "Processed"
+            and attachment.text_content
             else ""
             for attachment in self.attachment_set.all()
         ]
