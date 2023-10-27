74696639500ac0cbf0cf1209cd01fa664f141cba
PiotrIw <piotr.iwanski@gmail.com>
2023-10-27 11:49:46 +0000

    chore: skip getting letter Emaillabs status in development

--- feder/letters/logs/tasks.py
+++ feder/letters/logs/tasks.py
@@ -1,6 +1,7 @@
 import logging
 
 from background_task import background
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
@@ -10,6 +11,10 @@ def update_sent_letter_status():
     """
     Update the status of sent letters using Emaillabs API
     """
+    if settings.APP_MODE == "DEV":
+        logger.info("Skipping update_sent_letter_status task in DEV mode.")
+        return
+
     from feder.letters.logs.models import LogRecord
     from feder.letters.logs.utils import get_emaillabs_client
 
