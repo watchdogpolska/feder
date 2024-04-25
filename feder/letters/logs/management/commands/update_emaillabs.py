e7069c3a2c4022986ed2db31b9c7fdf0770ed5f5
PiotrIw <piotr.iwanski@gmail.com>
2024-04-25 19:03:42 +0000

    feat: add logging info

--- feder/letters/logs/management/commands/update_emaillabs.py
+++ feder/letters/logs/management/commands/update_emaillabs.py
@@ -12,6 +12,7 @@ class Command(BaseCommand):
     help = "Update the status of sent letters based on the Emaillabs API"
 
     def handle(self, *args, **options):
+        logger.info("Updating logs from emaillabs")
         client = get_emaillabs_client()
         skipped, saved = LogRecord.objects.parse_rows(client.get_emails_iter())
         logger.info(f"Saved {saved} new logs record and skipped {skipped} records.")
