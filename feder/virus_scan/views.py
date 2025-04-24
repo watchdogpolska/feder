a007042977db92941c6a98c1064d427bca5180eb
PiotrIw <piotr.iwanski@gmail.com>
2025-04-24 09:59:00 +0000

    fix: use metadefender callback as expected

--- feder/virus_scan/views.py
+++ feder/virus_scan/views.py
@@ -1,10 +1,11 @@
+import json
+
 from django.core.exceptions import SuspiciousOperation
 from django.http import JsonResponse
 from django.views.generic import View
 
 from .models import Request
 from .signer import TokenSigner
-from .tasks import scan_request
 
 
 class RequestWebhookView(View):
@@ -14,13 +15,31 @@ class RequestWebhookView(View):
         from .engine import get_engine
 
         current_engine = get_engine()
-        token = self.signer.unsign(self.request.GET.get("token", ""))
-        if token != current_engine.name:
+        token_engine = self.signer.unsign(self.request.GET.get("token", ""))
+        if token_engine != current_engine.name:
             raise SuspiciousOperation("Token does not match the current engine.")
-        for request in (
-            Request.objects.filter(status=Request.STATUS.queued)
-            .filter(engine_name=current_engine.name)
-            .all()
-        ):
-            scan_request(pk=request.pk)
+        headers = dict(request.headers)
+        try:
+            result = json.loads(request.body.decode("utf-8"))
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON body"}, status=400)
+        if not result.get("data_id"):
+            return JsonResponse({"error": "Missing data_id"}, status=400)
+        request = Request.objects.filter(
+            engine_id=result["data_id"], engine_name=current_engine.name
+        ).first()
+        if not request:
+            return JsonResponse({"error": "Request not found"}, status=404)
+        result["response_headers"] = headers
+        request_update = {
+            "engine_id": result["data_id"],
+            "status": current_engine.map_status(result),
+            "engine_report": result,
+            "engine_link": current_engine.get_result_url(
+                result["data_id"] if result["data_id"] is not None else None,
+            ),
+        }
+        for key in request_update:
+            setattr(request, key, request_update[key])
+        request.save()
         return JsonResponse({"status": "OK"})
