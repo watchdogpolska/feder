bb0441b067220451826d538cc49ca04e9e6f57c5
PiotrIw <piotr.iwanski@gmail.com>
2025-04-24 10:00:02 +0000

    fix: protocol for development and result url

--- feder/virus_scan/engine/base.py
+++ feder/virus_scan/engine/base.py
@@ -1,5 +1,6 @@
 import urllib.parse
 
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
 
@@ -7,12 +8,14 @@ from ..signer import TokenSigner
 
 
 class BaseEngine:
+    name = "BaseEngine"
+
     def __init__(self):
         self.signer = TokenSigner()
 
     def get_webhook_url(self):
         return "{}://{}{}?token={}".format(
-            "https",
+            "http" if getattr(settings, "APP_MODE", False) == "DEV" else "https",
             get_current_site(None).domain,
             reverse("virus_scan:webhook"),
             urllib.parse.quote(self.signer.sign(self.name)),
@@ -24,4 +27,9 @@ class BaseEngine:
     def receive_scan(self, engine_id):
         raise NotImplementedError(
             f"Provide 'receive_scan' in {self.__class__.__name__}"
+        )
+
+    def get_result_url(self, engine_id):
+        raise NotImplementedError(
+            f"Provide 'get_result_url' in {self.__class__.__name__}"
         )
