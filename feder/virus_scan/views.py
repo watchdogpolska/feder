deb73d0e8c9ea6c410d577d37875a664cca04934
PiotrIw <piotr.iwanski@gmail.com>
2025-04-24 12:01:19 +0000

    feat: add webhook logging

--- feder/virus_scan/views.py
+++ feder/virus_scan/views.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from django.core.exceptions import SuspiciousOperation
 from django.http import JsonResponse
@@ -6,6 +7,8 @@ from django.views.generic import View
 
 from .models import Request
 from .signer import TokenSigner
+
+logger = logging.getLogger(__name__)
 
 
 class RequestWebhookView(View):
@@ -19,16 +22,22 @@ class RequestWebhookView(View):
         if token_engine != current_engine.name:
             raise SuspiciousOperation("Token does not match the current engine.")
         headers = dict(request.headers)
+        logger.info(f"Received webhook from {current_engine.name}")
+        logger.info(f"Headers: {headers}")
+        logger.info(f"Body: {request.body.decode('utf-8')}")
         try:
             result = json.loads(request.body.decode("utf-8"))
         except json.JSONDecodeError:
+            logger.error("Invalid JSON body")
             return JsonResponse({"error": "Invalid JSON body"}, status=400)
         if not result.get("data_id"):
+            logger.error("Missing data_id in the request")
             return JsonResponse({"error": "Missing data_id"}, status=400)
         request = Request.objects.filter(
             engine_id=result["data_id"], engine_name=current_engine.name
         ).first()
         if not request:
+            logger.error(f"Request not found: {result["data_id"]}")
             return JsonResponse({"error": "Request not found"}, status=404)
         result["response_headers"] = headers
         request_update = {
@@ -42,4 +51,5 @@ class RequestWebhookView(View):
         for key in request_update:
             setattr(request, key, request_update[key])
         request.save()
+        logger.info(f"Request updated: {request}")
         return JsonResponse({"status": "OK"})
