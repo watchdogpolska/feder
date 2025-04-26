ed70f6d84ebacbc34c57baa30c1f5c4b6bda4d48
PiotrIw <piotr.iwanski@gmail.com>
2025-04-26 20:42:09 +0000

    refactor: isort

--- feder/virus_scan/admin.py
+++ feder/virus_scan/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from feder.letters.models import Letter
-from django.utils.translation import gettext_lazy as _
 from feder.virus_scan.models import Request
 
 
