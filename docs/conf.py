abc6d9cfcb6390daf55572f89cc4de5b6bb90495
Piotr <36678620+PiotrIw@users.noreply.github.com>
2026-01-12 23:09:34 +0100

    Refactor comment for clarity in conf.py
    Split a comment into two lines for better readability.

--- docs/conf.py
+++ docs/conf.py
@@ -313,7 +313,8 @@ def process_django_model(app, what, name, obj, options, lines):
             ):
                 rel = getattr(field, "related_model", None)
 
-                # On docs builds, relations can remain unresolved and be a string (lazy reference).
+                # On docs builds, relations can remain unresolved and be a string 
+                # (lazy reference).
                 if isinstance(rel, str):
                     lines.append(
                         f":type {field.attname}: {type(field).__name__} to ``{rel}``"
