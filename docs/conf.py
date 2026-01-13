61804457561a56df46d4e424d351f8d79a1c6c83
PiotrIw <piotr.iwanski@gmail.com>
2026-01-13 11:06:56 +0000

    remove doc duilding warnings

--- docs/conf.py
+++ docs/conf.py
@@ -126,6 +126,7 @@ try:
     import sphinx_rtd_theme
 
     html_theme = "sphinx_rtd_theme"
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 except ImportError:
     html_theme = "default"
