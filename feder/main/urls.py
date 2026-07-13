d2ee339e1e929647b045d3614c95d40e90016940
PiotrIw <piotr.iwanski@gmail.com>
2026-07-13 15:52:16 +0000

    allow to switch off debug_toolbar on local

--- feder/main/urls.py
+++ feder/main/urls.py
@@ -103,7 +103,7 @@ urlpatterns += [
     ),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
 
     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
